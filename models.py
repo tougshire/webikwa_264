a10150b0e3d356a1cbf0c9b0204b33c6d99d9779
tougshire <tougshire@tougshire.com>
2026-05-13 13:34:55 -0400

    Change summary to MarkdownField

--- models.py
+++ models.py
@@ -399,7 +399,7 @@ class ImageUrlHelpPanel(HelpPanel):
 class ArticlePage(BaseArticlePage):
 
     date = models.DateField("Post date", default=datetime.date.today)
-    summary = models.TextField(
+    summary = models.MarkdownField(
         blank=True,
         help_text="A summary to be displayed instead of the body for index views",
     )
